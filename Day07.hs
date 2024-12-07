61f3fae2b5cc9b87ee63d7cd6f171f054dfdbc3a
Stefan Holdermans <stefan@holdermans.nl>
2024-12-07 07:51:07 +0100

    Add solutions for Day 7

---
+++ Day07.hs
@@ -0,0 +1,33 @@
+-- Parsing --
+
+type Equation = (Int, [Int])
+
+type Equations = [Equation]
+
+parse :: String -> Equations
+parse = map (parseEq . words) . lines
+  where
+    parseEq (s : ss) = (read (init s), map read ss)
+
+-- Auxiliaries --
+
+solvable :: [Int -> Int -> Int] -> Equation -> Bool
+solvable ops (m, n : ns) = go n ns
+  where
+    go acc [] = acc == m
+    go acc (n : ns)
+      | acc > m = False
+      | otherwise = or [go (acc `op` n) ns | op <- ops]
+
+-- Part One --
+
+solve :: Equations -> Int
+solve = sum . map fst . filter (solvable ops) where ops = [(+), (*)]
+
+-- Part Two --
+
+(><) :: Int -> Int -> Int
+m >< n = read (show m ++ show n)
+
+solve' :: Equations -> Int
+solve' = sum . map fst . filter (solvable ops) where ops = [(+), (*), (><)]
