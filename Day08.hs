8d49f755afbb0da6bfbe831273066c13753a4636
Stefan Holdermans <stefan@holdermans.nl>
2024-12-08 09:27:37 +0100

    Add solutions for Day 8

---
+++ Day08.hs
@@ -0,0 +1,50 @@
+import Data.Ix (inRange, range)
+import Data.List (nub)
+import Data.Map qualified as Map (elems, fromListWith)
+
+-- Utilities --
+
+unique :: (Eq a) => [a] -> Int
+unique = length . nub
+
+-- Parsing --
+
+type Loc = (Int, Int)
+
+type Freq = [Loc]
+
+data Grid = Grid {bounds :: (Loc, Loc), freqs :: [Freq]}
+
+parse :: String -> Grid
+parse input = Grid bounds freqs
+  where
+    rows@(cols : _) = lines input
+    bounds = ((1, 1), (length rows, length cols))
+    cells = zip (concat rows) (range bounds)
+    antennas = Map.fromListWith (++) [(c, [pos]) | (c, pos) <- cells, c /= '.']
+    freqs = Map.elems antennas
+
+-- Auxiliaries --
+
+anti :: [Int] -> (Loc, Loc) -> [Loc]
+anti ks ((i, j), (i', j')) = [(i + k * di, j + k * dj) | k <- ks]
+  where
+    di = i' - i
+    dj = j' - j
+
+impact :: [Int] -> Grid -> Int
+impact ks (Grid bounds freqs) = unique locs
+  where
+    locs = concatMap (prune . anti ks) (concatMap pairs freqs)
+    pairs freq = [(loc, loc') | loc <- freq, loc' <- freq, loc /= loc']
+    prune = takeWhile (inRange bounds)
+
+-- Part One --
+
+solve :: Grid -> Int
+solve = impact [2]
+
+-- Part Two --
+
+solve' :: Grid -> Int
+solve' = impact [1 ..]
