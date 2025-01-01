b27f98fd8487ab50b76dc2a368e745a1cd3cc539
Stefan Holdermans <stefan@holdermans.nl>
2025-01-01 13:18:42 +0100

    Add solution for Day 25

---
+++ Day25.hs
@@ -0,0 +1,44 @@
+import Data.List (transpose)
+
+-- Utilities --
+
+split :: (a -> Bool) -> [a] -> [[a]]
+split p xs = case break p xs of
+  ([], []) -> []
+  ([], _ : zs) -> split p zs
+  (ys, []) -> [ys]
+  (ys, _ : zs) -> ys : split p zs
+
+-- Parsing --
+
+type Height = Int
+
+data Lockbit = Lock [Height] | Key [Height] deriving (Show)
+
+type Lockware = [Lockbit]
+
+parse :: String -> Lockware
+parse input = map pLockbit (split null (lines input))
+  where
+    pLockbit (('#' : _) : ss) = pLock (transpose (init ss))
+    pLockbit (('.' : _) : ss) = pKey (transpose (init ss))
+    pLock = Lock . map pHeight
+    pKey = Key . map (pHeight . reverse)
+    pHeight = length . takeWhile (== '#')
+
+-- Part One --
+
+fit :: Lockbit -> Lockbit -> Bool
+fit (Lock lhs) (Key khs) = all (<= 5) (zipWith (+) lhs khs)
+fit _ _ = False
+
+solve1 :: Lockware -> Int
+solve1 ss = length [(s1, s2) | s1 <- ss, s2 <- ss, fit s1 s2]
+
+-- Entry --
+
+main :: IO ()
+main = do
+  input <- getContents
+  let prob = parse input
+  print (solve1 prob)
